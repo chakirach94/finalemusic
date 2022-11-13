8c198a2e09d6b966336f4a827b92033540126c05
chakirach94 <95524156+chakirach94@users.noreply.github.com>
2022-11-13 21:45:35 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,58 @@
 from typing import Union
 from fastapi import FastAPI
+import requests
+import json
 
 app = FastAPI()
 
+def getlist(hliwa):
+  querystrings = {"q":hliwa}
+  headerss = {
+    "X-RapidAPI-Key": "68a49ac1a2msh3a7b4896a584357p137023jsn9db99d40833e",
+    "X-RapidAPI-Host": "youtube-search-results.p.rapidapi.com"
+  }
+  urls = "https://youtube-search-results.p.rapidapi.com/youtube-search/"
+  responses = requests.request("GET", urls, headers=headerss, params=querystrings)
+  jess_dict2 = json.loads(responses.text)
+  return jess_dict2
+
+def getlinkfromid(id):
+  url = "https://ytstream-download-youtube-videos.p.rapidapi.com/dl"
+  querystring = {"id":id}
+  headers = {
+    "X-RapidAPI-Key": "68a49ac1a2msh3a7b4896a584357p137023jsn9db99d40833e",
+    "X-RapidAPI-Host": "ytstream-download-youtube-videos.p.rapidapi.com"
+  }
+  response2 = requests.request("GET", url, headers=headers, params=querystring)
+  jess_dict2 = json.loads(response2.text)
+  return jess_dict2['formats'][2]['url']
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/searsh/{item_id}")
+@app.get("/search/songs/{item_id}")
 def searsh(item_id: str, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+        mylist=getlist(item_id)['items']
+        i=0
+        finalelist=[]
+        while (i<10 or i>len(mylist)):
+            if mylist[i]['type']=='video':
+                mine={"songid":mylist[i]['id'],
+                "songname":mylist[i]['title'],
+                "userid":mylist[i]['author']['channelID'],
+                "trackid":mylist[i]['id'],
+                "duration":str(mylist[i]['duration']),
+                "cover_image_url":mylist[i]['bestThumbnail']['url'],
+                "first_name":mylist[i]['author']['name'],
+                "last_name":str(mylist[i]['views'])
+                }
+                
+                finalelist.append(mine)
+            i=i+1
+            print(finalelist)
+        return {"results":finalelist }
 
 
 @app.get("/artist/{item_id}")
