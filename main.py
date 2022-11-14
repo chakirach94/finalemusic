7c68a7df2742f212225a4c782cdcc0551bea15e6
chakirach94 <95524156+chakirach94@users.noreply.github.com>
2022-11-14 19:22:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -58,8 +58,9 @@ def searsh(item_id: str, q: Union[str, None] = None):
 
 @app.get("/gotolink/{item_id}")
 async def gotolink(item_id: str, q: Union[str, None] = None):
-    response = RedirectResponse(url='https://redirector.googlevideo.com/videoplayback?expire=1668469041&ei=0HxyY5-ZLPCP2LYPlIG-iAo&ip=198.98.59.215&id=o-AHn0JsC6v3iZM03yojnwR9LFlRTKr_l5rU5g_yX0rnb5&itag=18&source=youtube&requiressl=yes&mh=D9&mm=31%2C29&mn=sn-ab5sznzd%2Csn-ab5l6nrs&ms=au%2Crdu&mv=m&mvi=1&pl=24&initcwndbps=93750&spc=SFxXNumFP2NhDU7yEwGDc7-aUQxs8SGs3Z1iFz4GaLaD&vprv=1&mime=video%2Fmp4&ns=ecWE6WCf72MEfufspWFhMZAJ&gir=yes&clen=14088459&ratebypass=yes&dur=199.936&lmt=1666358220522542&mt=1668446943&fvip=2&fexp=24001373%2C24007246&c=WEB&txp=4530434&n=N73YGZbZ774bAg&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cspc%2Cvprv%2Cmime%2Cns%2Cgir%2Cclen%2Cratebypass%2Cdur%2Clmt&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Cinitcwndbps&lsig=AG3C_xAwRgIhAP_Y6liefYjEidUL4CEDKZqRUxdv2kHdZTjCyWucmHiRAiEArT9EbytDJMeqBKbi9gamQ2WvZR40tCm1SS0OJPesYPM%3D&sig=AOq0QJ8wRgIhAL3K6tBgeWOvBTlvJphMALBicFJheLDPZAVBwqxK6D8fAiEAmaSy2r0ONcrTzcO-Dx_jUkBy8n40E8l34K2L6RojcVY%3D&range=0-',status_code=303)
-    return response
+  link=getlinkfromid(item_id)
+  response = RedirectResponse(url=link,status_code=303)
+  return response
 
 @app.get("/album/{item_id}")
 def album(item_id: str, q: Union[str, None] = None):
