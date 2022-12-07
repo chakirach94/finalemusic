fe6428a47de420a74e4bbe729f35a9c1d985f7ff
chakirach94 <95524156+chakirach94@users.noreply.github.com>
2022-12-07 17:07:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -257,34 +257,6 @@ async def gotolink(item_id: str, q: Union[str, None] = None):
         response = RedirectResponse(url="https://drive.google.com/uc?id=1ixtBSHFEMH9zyDkRNLU79hYSpA5ZHAd&export=download",status_code=303)
 
     return response
-
- def channelsearch(channel):
-  url = "https://youtube-media-downloader.p.rapidapi.com/v2/search/channels"
-
-  querystring = {"keyword":channel,"sortBy":"relevance"}
-
-  headers = {
-    "X-RapidAPI-Key": "68a49ac1a2msh3a7b4896a584357p137023jsn9db99d40833e",
-    "X-RapidAPI-Host": "youtube-media-downloader.p.rapidapi.com"
-  }
-
-  response = requests.request("GET", url, headers=headers, params=querystring)
-  jess_dict2 = json.loads(response.text)
-  if jess_dict2['status']==True:
-      return jess_dict2
-  return "false"
-
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/get/profil/{item_id}")
-def searsh(item_id: str, q: Union[str, None] = None):
-        
-        return {"results":{item_id} }
     
     
 @app.get("/album/{item_id}")
